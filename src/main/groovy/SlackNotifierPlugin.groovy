76f575eaad27b1a20884b64d60dfbd50cca65f1a
Bouwdie <peter.bouwdewijn@gmail.com>
2017-06-24 17:17:32 +0200

    Create LICENCE

--- src/main/groovy/SlackNotifierPlugin.groovy
+++ src/main/groovy/SlackNotifierPlugin.groovy
@@ -1,3 +1,8 @@
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import com.dtolabs.rundeck.plugins.notification.NotificationPlugin
 import com.fasterxml.jackson.databind.ObjectMapper
 
